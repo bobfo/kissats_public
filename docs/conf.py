61402778a85212841202f4c126ac1e08ace2de88
Bob Folkes <bob@folkes.us>
2018-07-16 01:22:53 -0500

    update conf for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ author = u'Bob Folkes'
 
 
 # The short X.Y version
-version = 1.0
+version = '1.0.0a1'
 # The full version, including alpha/beta/rc tags
-release = 1.0.0a1
+release = '1.0.0a1'
 
 
 # -- General configuration ---------------------------------------------------
